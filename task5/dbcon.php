11eaf46f162dec5224370d0338ee9e43485654f0
turshinashtary <78007620+turshinashtary@users.noreply.github.com>
2022-04-29 14:37:42 +0600

    Delete task5 directory

--- task5/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $db = "dbms";
-
-    $con = mysqli_connect($server,$user,$password,$db);
-?>
--- task5/logout.php
+++
@@ -1,9 +0,0 @@
-<?php  
-session_start();
-if (isset($_SESSION['username'])) {
-	session_destroy();
-	header("location:login.php");
-}else{
-	header("location:login.php");
-}
-?>
